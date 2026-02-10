8764cd5e99d2e8d66dbd02c255e902d405c3075f
George <119165350+Viso273@users.noreply.github.com>
2026-02-10 23:46:11 +0800

    Delete LeetCodeHoot100/src directory

--- LeetCodeHoot100/src/T46/Test.java
+++
@@ -1,9 +0,0 @@
-package T46;
-
-public class Test {
-    public static void main(String[] args) {
-        int[] nums = {1,2,3};
-        Solution solution = new Solution();
-        System.out.println(solution.permute(nums));
-    }
-}
