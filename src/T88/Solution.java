9fdf7a7bd52f9b22d4451c2f8bdb00b9faee0ee1
George <119165350+Viso273@users.noreply.github.com>
2026-02-12 22:51:50 +0800

    Add files via upload
    双指针合并两个数组

--- src/T88/Solution.java
+++ src/T88/Solution.java
@@ -1,4 +1,7 @@
 package T88;
+
+import java.util.Arrays;
+
 //给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
 //请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
 //注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
@@ -22,4 +25,32 @@ package T88;
 //合并结果是 [1] 。
 //注意，因为 m = 0 ，所以 nums1 中没有元素。nums1 中仅存的 0 仅仅是为了确保合并结果可以顺利存放到 nums1 中。
 public class Solution {
+    // 暴力解，将 nums2 中的元素添加到 nums1 中，然后排序
+    //    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    //        for (int i = 0; i < n; i++){
+    //            nums1[m + i] = nums2[i];
+    //        }
+    //        Arrays.sort(nums1);
+    //    }
+
+    // 双指针解法，从后往前遍历，比较 nums1 和 nums2 中的元素，将较大的元素放到 nums1 的末尾
+    public void merge(int[] nums1, int m, int[] nums2, int n){
+        int p1 = m - 1;// nums1 的指针
+        int p2 = n - 1;// nums2 的指针
+        int p = m + n - 1;// nums1 的末尾指针
+        while (p1 >= 0 && p2 >= 0){
+            if (nums1[p1] > nums2[p2]){
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
+            }
+            p--;
+        }
+        // 如果 nums2 中还有剩余元素，说明这些元素都比 nums1 中的元素小，需要将它们放到 nums1 的前面
+        if (p2 >= 0){
+            System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
+        }
+    }
 }
--- src/T88/Test.java
+++ src/T88/Test.java
@@ -1,4 +1,15 @@
 package T88;
 
 public class Test {
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int n = 3;
+        Solution solution = new Solution();
+        solution.merge(nums1, m, nums2, n);
+        for (int num : nums1){
+            System.out.print(num + " ");
+        }
+    }
 }
