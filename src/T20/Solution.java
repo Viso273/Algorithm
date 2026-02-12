802c9897dc470bd9c3cba014ae3b7e48472bf2f2
George <119165350+Viso273@users.noreply.github.com>
2026-02-12 22:51:13 +0800

    Add files via upload
    栈实现符号匹配

---
+++ src/T20/Solution.java
@@ -0,0 +1,55 @@
+package T20;
+//给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，
+//判断字符串是否有效。
+//有效字符串需满足：
+//左括号必须用相同类型的右括号闭合。
+//左括号必须以正确的顺序闭合。
+//每个右括号都有一个对应的相同类型的左括号。
+//
+//示例 1：
+//输入：s = "()"
+//输出：true
+//
+//示例 2：
+//输入：s = "()[]{}"
+//输出：true
+//
+//示例 3：
+//输入：s = "(]"
+//输出：false
+//
+//示例 4：
+//输入：s = "([])"
+//输出：true
+//
+//示例 5：
+//输入：s = "([)]"
+//输出：false
+
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> deque = new LinkedList<>();
+        char ch;
+        for (int i = 0; i < s.length(); i++) {
+            ch = s.charAt(i);
+            //碰到左括号，就把相应的右括号入栈
+            if (ch == '(') {
+                deque.push(')');
+            }else if (ch == '{') {
+                deque.push('}');
+            }else if (ch == '[') {
+                deque.push(']');
+            } else if (deque.isEmpty() || deque.peek() != ch) {//如果是右括号判断是否和栈顶元素匹配
+                return false;
+            }else {//如果匹配就把栈顶元素出栈
+                deque.pop();
+            }
+        }
+        //最后判断栈中元素是否匹配
+        return deque.isEmpty();
+    }
+}
---
+++ src/T20/Test.java
@@ -0,0 +1,9 @@
+package T20;
+
+public class Test {
+    public static void main(String[] args) {
+        String s = "([)]";
+        Solution solution = new Solution();
+        System.out.println(solution.isValid(s));
+    }
+}
