b1c49e914b5c24a3c2cd20e5d1610784faa64b96
Zlang <1286388386@qq.com>
2026-02-12 23:10:28 +0800

    贪心解决最大利润问题

---
+++ src/T121/Test.java
@@ -0,0 +1,9 @@
+package T121;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(sol.maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
+}
