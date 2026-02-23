8a165e78ab9631e447bd9064fee4304929edf6b2
Zlang <1286388386@qq.com>
2026-02-23 18:18:49 +0800

    二叉树的锯齿形层次遍历

---
+++ src/T103/Test.java
@@ -0,0 +1,17 @@
+package T103;
+
+import Objects.TreeNode;
+
+public class Test {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        Solution solution = new Solution();
+        solution.zigzagLevelOrder(root);
+        System.out.println(solution.zigzagLevelOrder(root));
+    }
+}
