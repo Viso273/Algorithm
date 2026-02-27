6535334a9ef21f08c92b485586f42d6a58ac9f64
Zlang <1286388386@qq.com>
2026-02-27 22:06:37 +0800

    动态规划找数组中的最长严格递增子序列长度

---
+++ src/T300/Test.java
@@ -0,0 +1,9 @@
+package T300;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLIS(nums));
+    }
+}
