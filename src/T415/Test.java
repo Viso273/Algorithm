ac22232dd6a4d24c13d2fd37fed5bf04356adf2f
Zlang <1286388386@qq.com>
2026-03-02 23:37:29 +0800

    字符串相加

---
+++ src/T415/Test.java
@@ -0,0 +1,8 @@
+package T415;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.addStrings("11", "123"));
+    }
+}
