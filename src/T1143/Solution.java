7dc7d23f11ecc0a5e6a0ff1648c63259c25ac062
Zlang <1286388386@qq.com>
2026-03-07 16:52:07 +0800

    动态规划解决最长公共子序列

---
+++ src/T1143/Solution.java
@@ -0,0 +1,50 @@
+package T1143;
+//给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。
+//如果不存在 公共子序列 ，返回 0 。
+//一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符
+//（也可以不删除任何字符）后组成的新字符串。
+//例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+//两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+//
+//示例 1：
+//输入：text1 = "abcde", text2 = "ace"
+//输出：3
+//解释：最长公共子序列是 "ace" ，它的长度为 3 。
+//
+//示例 2：
+//输入：text1 = "abc", text2 = "abc"
+//输出：3
+//解释：最长公共子序列是 "abc" ，它的长度为 3 。
+//
+//示例 3：
+//输入：text1 = "abc", text2 = "def"
+//输出：0
+//解释：两个字符串没有公共子序列，返回 0 。
+public class Solution {
+    // 动态规划
+    // 定义dp数组
+    // dp[i][j]表示text1的前i个元素组成的串和text2的前j个元素组成的串之间的最长公共子序列
+    // 初始化：dp[0][j] 和 dp[i][0]都是0
+    // 递推公式：对于dp[i][j]，考察text1[i-1]与text2[j-1]
+    // 当text1[i-1]与text2[j-1]相同 dp[i][j] = dp[i-1][j-1]+1
+    // 当text1[i-1]与text2[j-1]不同 dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1])                                   取dp[i-1][j]和dp[i][j-1]的最大值
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        int[][] dp = new int[m+1][n+1];
+
+        for (int i = 1; i < m+1; i++) {
+            char c1 = text1.charAt(i-1);
+            for (int j = 1; j < n+1; j++) {
+                char c2 = text2.charAt(j-1);
+                if(c1==c2){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
---
+++ src/T1143/Test.java
@@ -0,0 +1,10 @@
+package T1143;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String text1 = "abcde";
+        String text2 = "ace";
+        System.out.println(solution.longestCommonSubsequence(text1,text2));
+    }
+}
