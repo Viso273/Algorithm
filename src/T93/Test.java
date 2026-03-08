6d035ed0bda734c61ad3dc5f63fe5ba2553ad0d7
Zlang <1286388386@qq.com>
2026-03-08 21:06:00 +0800

    回溯解决复原ip地址

---
+++ src/T93/Test.java
@@ -0,0 +1,10 @@
+package T93;
+
+public class Test {
+    public static void main(String[] args) {
+        String s = "25525511135";
+        Solution solution = new Solution();
+        solution.restoreIpAddresses(s);
+        System.out.println(solution.result);
+    }
+}
