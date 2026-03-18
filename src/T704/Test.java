7b79f3bc1b444fb95be424db77f7ef6b169542de
Zlang <1286388386@qq.com>
2026-03-18 23:59:57 +0800

    二分排序

---
+++ src/T704/Test.java
@@ -0,0 +1,8 @@
+package T704;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1,0,3,5,9,12};
+        System.out.println(Solution.search(nums,9));
+    }
+}
