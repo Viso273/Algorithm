aea8cc5ddbc018ec8e8ea6ff73791af1df02ca23
Zlang <1286388386@qq.com>
2026-04-18 09:43:42 +0800

    在排序数组中查找元素的第一个和最后一个位置，使用二分查找找到第一个，然后将目标值加1找到比目标值大一的第一个位置，然后减一得到目标值的最后一个位置

--- src/T34/Solution.java
+++ src/T34/Solution.java
@@ -35,7 +35,7 @@ class Solution {
         int left = 0;
         int right = nums.length-1;
         while(left<=right){
-            int mid = left + (right - left)/2;
+            int mid = (right + left)/2;
             if(nums[mid]>=target) {
                 right = mid - 1;
             }else{
